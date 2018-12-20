aa34cffe4d82559d46f3d6a7f2b281487e9bd210
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-20 10:54:32 +0100

    TP 02

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomed = new Random().Next(49) + 1; // nombre Aléatoire
+            int numberTry = 0;
+            bool find = false;
+            Console.WriteLine("Saisissez un nombre entre 1 et 50 :");
+            while (!find)
+            {
+                string picked = Console.ReadLine();
+                int pickedValue;
+                if (int.TryParse(picked, out pickedValue))
+                {
+                    if (pickedValue == randomed)
+                        find = true; // on sort de la boucle car find = true
+                    else
+                    {
+                        if (pickedValue < randomed)
+                            Console.WriteLine("Trop petit ...");
+                        else
+                            Console.WriteLine("Trop grand ...");
+                    }
+                    numberTry++;
+                }
+                else
+                    Console.WriteLine("Le nombre saisie est incorrecte, veuillez recommencer ...");
+            }
+            if (numberTry == 1)
+                Console.WriteLine("BRAVO ! Vous avez trouvé en " + numberTry + " coup");
+            else
+                Console.WriteLine("BRAVO ! Vous avez trouvé en " + numberTry + " coups");
+        }
+    }
+}
